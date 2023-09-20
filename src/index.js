c40db694ac49fed2ccac9964e2e069d9939988f6
dngo-extron <dngo@extron.com>
2023-09-20 09:51:16 -0700

    update

---
+++ src/index.js
@@ -0,0 +1,5 @@
+//@ts-check
+"use strict";
+import m from "mithril";
+
+m.render( document.body, "hello world" );
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: 'index.html'})
+  ],
+};
